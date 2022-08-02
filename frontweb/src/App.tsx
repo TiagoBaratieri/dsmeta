9e39b6e9a1bc451254b02b802364b409b32df82c
Tiago Baratieri <tiagobaratieri97@gmail.com>
2022-08-02 12:26:49 -0300

    NotificationButton

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import NotificationButton from './components/NotificationButton';
 
 function App() {
   return (
     <div className="App">
      <Header />
+     <NotificationButton />
     </div>
   );
 }
