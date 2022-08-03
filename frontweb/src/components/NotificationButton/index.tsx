9417c478f78f697d5b3f4abaab98c059b37cbdd0
Tiago Baratieri <tiagobaratieri97@gmail.com>
2022-08-03 13:58:19 -0300

    Enviar notificação

--- frontweb/src/components/NotificationButton/index.tsx
+++ frontweb/src/components/NotificationButton/index.tsx
@@ -1,10 +1,21 @@
+import axios from "axios";
 import icon from "../../assets/img/notification-icon.svg";
+import { BASE_URL } from "../../utils/request";
 import "./styles.css";
 
-const NotificationButton = () => {
+type Props = {
+  saleId: number;
+};
+
+function handleClick(id: number) {
+  axios(`${BASE_URL}/sales/${id}/notification`).then((reponse) => {
+    console.log("Sucesso");
+  });
+}
+const NotificationButton = ({ saleId }: Props) => {
   return (
     <div className="dsmeta-red-btn">
-      <img src={icon} alt="Notificar" />
+      <img src={icon} alt="Notificar" onClick={() => handleClick(saleId)} />
     </div>
   );
 };
